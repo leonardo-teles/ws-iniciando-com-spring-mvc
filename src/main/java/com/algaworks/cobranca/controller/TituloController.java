b7d55e46767dcaa6a669cbf0654542a49633539d
Leonardo Almeida <leonardo.teles.almeida@gmail.com>
2018-08-08 20:47:28 -0300

    Implementando a Edição

--- src/main/java/com/algaworks/cobranca/controller/TituloController.java
+++ src/main/java/com/algaworks/cobranca/controller/TituloController.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.validation.Errors;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
@@ -21,16 +22,30 @@ import com.algaworks.cobranca.repository.Titulos;
 @RequestMapping("/titulos")
 public class TituloController {
 	
+	private static final String CADASTRO_VIEW = "cadastroTitulo";
+	
 	@Autowired
 	private Titulos titulos;
 
 	@RequestMapping("/novo")
 	public ModelAndView novo() {
-		ModelAndView mv = new ModelAndView("cadastroTitulo");
+		ModelAndView mv = new ModelAndView(CADASTRO_VIEW);
 		mv.addObject("statusTitulo", StatusTitulo.values());
 		mv.addObject(new Titulo());
 		
 		return mv;
+	}
+
+	@RequestMapping(method = RequestMethod.POST)
+	public String salvar(@Validated Titulo titulo, Errors errors, RedirectAttributes attributes) {
+		if (errors.hasErrors()) {
+			return CADASTRO_VIEW;
+		}
+		
+		titulos.save(titulo);
+		attributes.addFlashAttribute("mensagem", "Título salvo com sucesso");
+		
+		return "redirect:/titulos/novo";
 	}
 	
 	@RequestMapping
@@ -42,16 +57,12 @@ public class TituloController {
 		return mv;
 	}
 	
-	@RequestMapping(method = RequestMethod.POST)
-	public String salvar(@Validated Titulo titulo, Errors errors, RedirectAttributes attributes) {
-		if (errors.hasErrors()) {
-			return "cadastroTitulo";
-		}
+	@RequestMapping("{codigo}")
+	public ModelAndView edicao(@PathVariable("codigo") Titulo titulo) {
+		ModelAndView mv = new ModelAndView(CADASTRO_VIEW);
+		mv.addObject(titulo);
 		
-		titulos.save(titulo);
-		attributes.addFlashAttribute("mensagem", "Título salvo com sucesso");
-		
-		return "redirect:/titulos/novo";
+		return mv;
 	}
 	
 	@ModelAttribute("statusTitulo")
