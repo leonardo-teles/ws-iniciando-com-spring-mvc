b1aab2cd2c4659324ec0e35e0c6da58141d9bc0e
Leonardo Almeida <leonardo.teles.almeida@gmail.com>
2018-08-13 19:24:43 -0300

    Corrigindo BUG Exclusão

--- src/main/resources/static/js/cobranca.js
+++ src/main/resources/static/js/cobranca.js
@@ -4,13 +4,17 @@ $('#confirmacaoExclusaoModal').on('show.bs.modal', function(event) {
 	
 	var codigoTitulo = button.data('codigo');
 	var descricaoTitulo = button.data('descricao');
-	
-	 /*Estre trecho de código faz uma validação no formulário para saber se a requisição termina ou não com (/{codigo}) a ser excluído
-	  *Caso ocorra algum problema na inclusão da barra(/) e do códgio({5}) a ser excluído, este trecho inclui os dois valores 
-	 */
+
+	/*
+	* Estre trecho de código faz uma validação no formulário
+	* para saber se a requisição termina ou não com (/{codigo})
+	* a ser excluído Caso ocorra algum problema na inclusão da
+    * barra(/) e do códgio({5}) a ser excluído, este trecho
+	* inclui os dois valores
+	*/
 	var modal = $(this);
 	var form = modal.find('form');
-	var action = form.attr('action');
+	var action = form.data('url-base');
 	if(!action.endsWith('/')) {
 		action += '/';
 	}
