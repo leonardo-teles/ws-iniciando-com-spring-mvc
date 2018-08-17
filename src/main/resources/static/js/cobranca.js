f3d87f8501fc8d0125bdeeb052457cd77ab89207
Leonardo Almeida <leonardo.teles.almeida@gmail.com>
2018-08-17 00:20:52 -0300

    Recebendo Título com Ajax - Parte 2

--- src/main/resources/static/js/cobranca.js
+++ src/main/resources/static/js/cobranca.js
@@ -34,5 +34,22 @@ $(function() {
 		var botaoReceber = $(event.currentTarget);
 		var urlReceber = botaoReceber.attr('href');
 		
+		var response = $.ajax({
+			url: urlReceber,
+			type: 'PUT'
+		});
+		
+		response.done(function(e) {
+			var codigoTitulo = botaoReceber.data('codigo');
+			$('[data-role=' + codigoTitulo + ']').html('<span class="label label-success">' + e + '</span>');
+			
+			botaoReceber.hide();
+		});
+		
+		response.fail(function(e) {
+			console.log(e);
+			alert('Erro recebendo cobrança');
+		});
+		
 	});
 }); 
